b2e1228258d731ff37ce50dacd710f210758efc8
Gustavo Santos <root.gusta@gmail.com>
2026-06-16 21:35:11 -0300

    feat: implement room CRUD endpoints

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using RoomManagerAPI.Data;
+using RoomManagerAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,62 @@ using (var scope = app.Services.CreateScope())
     db.Database.Migrate();
 }
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/salas", async (AppDbContext db) =>
+{
+    return await db.SalasReuniao.ToListAsync();
+});
+
+app.MapPost("/salas", async (
+    AppDbContext db,
+    SalaReuniao sala
+) =>
+{
+    db.SalasReuniao.Add(sala);
+
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/salas/{sala.Id}", sala);
+});
+
+app.MapPut("/salas/{id}", async (
+    int id,
+    AppDbContext db,
+    SalaReuniao sala
+) =>
+{
+    var salaExistente = await db.SalasReuniao.FindAsync(id);
+
+    if (salaExistente == null)
+    {
+        return Results.NotFound();
+    }
+
+    salaExistente.Nome = sala.Nome;
+    salaExistente.Capacidade = sala.Capacidade;
+    salaExistente.PossuiProjetor = sala.PossuiProjetor;
+
+    await db.SaveChangesAsync();
+
+    return Results.Ok(salaExistente);
+});
+
+app.MapDelete("/salas/{id}", async (
+    int id,
+    AppDbContext db
+) =>
+{
+    var sala = await db.SalasReuniao.FindAsync(id);
+
+    if (sala == null)
+    {
+        return Results.NotFound();
+    }
+
+    db.SalasReuniao.Remove(sala);
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
 
 app.Run();
